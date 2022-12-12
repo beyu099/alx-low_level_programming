da1decd8fe476887057d41c6a1033939eab8a68f
beyu099 <firstadebayo@gmail.com>
2022-12-12 08:44:09 -0800

    last exercise

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - The description is practising file size
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	
+	printf ("Size of a char:Size of a char: %lu byte(S)\n", (unsigned long)sizeof (a));
+	printf ("Size of a char:Size of an int: %lu byte(S)\n", (unsigned long)sizeof (b));
+	printf ("Size of a char:Size of a long int: %lu byte(S)\n", (unsigned long)sizeof (c));
+	printf ("Size of a char:Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof (d));
+	printf ("Size of a char:Size of a float: %lu byte(S)\n", (unsigned long)sizeof (f));
+	return 0;
+}
