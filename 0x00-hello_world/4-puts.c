6086721b705d7ab3da66be6d368995e7f6dfe110
beyu099 <firstadebayo@gmail.com>
2022-12-12 06:56:14 -0800

    4-puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+#include <stdio.h>
+/** 
+ * main - main block
+ * return: 0
+ */
 
 int main(void)
-
 {
-	    puts("/"Programming is like building a multilingual puzzle");
-	    return (0);
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
 }
