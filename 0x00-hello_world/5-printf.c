5156335e8d98fe6739341b9fb008f45b37500486
beyu099 <firstadebayo@gmail.com>
2022-12-12 08:15:13 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the description
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
