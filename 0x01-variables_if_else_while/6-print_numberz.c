6c6d87bb43891c1ecef3d2d8eb802e4ce2cfc0a6
beyu099 <firstadebayo@gmail.com>
2022-12-14 02:45:53 -0800

    base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing base numbers
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n')
+	return (0);
+}
