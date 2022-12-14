ccaec29914721f63ff54a54357332e060a6f260b
beyu099 <firstadebayo@gmail.com>
2022-12-14 03:01:56 -0800

    alphabet and number

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers of base sixteen
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
