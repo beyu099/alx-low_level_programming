7c63c08e8a24014190e2786b4d56589e08a1cb64
beyu099 <firstadebayo@gmail.com>
2022-12-18 14:25:04 -0800

    multiplication through 1-9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - checks through 0-9 for digits
+ * @a: variable - a
+ * @b: variable - b
+ * Return 0
+ */
+
+int mul(inta, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
