aaad08f5909015ce5f845375e69cebf6cc04ed1b
beyu099 <firstadebayo@gmail.com>
2022-12-18 21:24:29 -0800

    printing through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Print Number: Checks through 0-9 for numbers
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
