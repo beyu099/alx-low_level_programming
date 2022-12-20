4a6e3d22ceb97b7e14566eb68e97d93eb9ac5441
beyu099 <firstadebayo@gmail.com>
2022-12-20 15:20:09 -0800

    Prints the lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string
+ * @s: string
+ *
+ * Return: length
+ */
+
+int_strlen(char *s)
+{
+	int long = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
