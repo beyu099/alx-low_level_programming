d044cc36cac9f6a11d5f24a7580d9bc5e63d501e
beyu099 <firstadebayo@gmail.com>
2022-12-20 16:18:26 -0800

    prints one character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#input "main.h"
+
+/**
+ * puts2 - function should print one character off two characters
+ * @str: input
+ * Return: 0;
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		long++;
+	}
+	t = long - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			if (o % 2 == 0)
+			{
+				_putchar(str[o]);
+			}
+		}
+		_putchar('\n');
+	}
