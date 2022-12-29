a0863ee962837ed6e5863cf2924d4e595291a6c2
beyu099 <firstadebayo@gmail.com>
2022-12-29 02:47:02 -0800

    Updated: compiler errorsolved:

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		-putchar(letter);
+
+	_putchar('\n');
+}
